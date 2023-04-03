76aff74f702c78c2e43999767a34e6d8ede74771
uyudane <sy.yun@icloud.com>
2023-04-03 16:12:37 +0900

    Add: -8章

--- envs/prod/cicd/app_foobar/iam.tf
+++ envs/prod/cicd/app_foobar/iam.tf
@@ -48,3 +48,76 @@ resource "aws_iam_role_policy_attachment" "role_deployer_policy_ecr_power_user" 
   role       = aws_iam_role.deployer.name
   policy_arn = data.aws_iam_policy.ecr_power_user.arn
 }
+
+# GitHub Actions 上で実行させる ecspresso に tfstate(S3 オブジェクト) を参照させるための読み取り権限
+# 該当の tfstate 読み取り権限をインライン ポリシーとして作成し、デプロイ用の IAM ロールにアタッチ
+resource "aws_iam_role_policy" "s3" {
+  name = "s3"
+  role = aws_iam_role.deployer.id
+
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "s3:GetObject"
+          ],
+          "Resource" : "arn:aws:s3:::shonansurvivors-tfstate/${local.system_name}/${local.env_name}/cicd/app_${local.service_name}_*.tfstate"
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "s3:PutObject"
+          ],
+          "Resource" : "${data.aws_s3_bucket.env_file.arn}/*"
+        },
+      ]
+    }
+  )
+}
+
+# デプロイ用 IAM ロールに、サービスやタスク定義を更新する権限を追加
+resource "aws_iam_role_policy" "ecs" {
+  name = "ecs"
+  role = aws_iam_role.deployer.id
+
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "RegisterTaskDefinition",
+          "Effect" : "Allow",
+          "Action" : [
+            "ecs:RegisterTaskDefinition"
+          ],
+          "Resource" : "*"
+        },
+        {
+          "Sid" : "PassRolesInTaskDefinition",
+          "Effect" : "Allow",
+          "Action" : [
+            "iam:PassRole"
+          ],
+          "Resource" : [
+            data.aws_iam_role.ecs_task.arn,
+            data.aws_iam_role.ecs_task_execution.arn,
+          ]
+        },
+        {
+          "Sid" : "DeployService",
+          "Effect" : "Allow",
+          "Action" : [
+            "ecs:UpdateService",
+            "ecs:DescribeServices"
+          ],
+          "Resource" : [
+            data.aws_ecs_service.this.arn
+          ]
+        }
+      ]
+    }
+  )
+}
